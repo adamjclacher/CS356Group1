4a213366711b70bd03d39508e92bab445bb23b4a
Jamie Connelly <itsphireyt@gmail.com>
2024-05-24 17:34:30 +0100

    SCRUM-50 Some smaller naming changes

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,4 +1,8 @@
-function helloWorld() {
+/**
+ * displayPageOptions
+ * Used for debugging purposes at the moment
+ */
+function displayPageOptions() {
     const options = {
         projectName: document.getElementById("projectName").value,
         radioOption: document.querySelector('input[name="radioOption"]:checked').value
