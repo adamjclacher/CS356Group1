cb9a5501fdd1b64c235f8f02018e84d6652e38cb
Jamie Connelly <itsphireyt@gmail.com>
2024-05-24 15:34:27 +0100

    SCRUM-50 Basic setup, moving to PC...

--- src/Basic Flask.py
+++ src/Basic Flask.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder="templates/")
 
 
-# Root Index Route
 @app.route("/", methods=["GET"])
 def root():
     return render_template("index.html")
