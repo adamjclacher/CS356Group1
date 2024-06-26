1a7e34a5c26c2b1c73c34baac212baac6c192daa
JamesBrown1 <james.brown.2021@uni.strath.ac.uk>
2024-06-26 12:28:47 +0100

    feature/SCRUM-82 adding filename_fixer

--- src/app.py
+++ src/app.py
@@ -92,7 +92,7 @@ def get_config():
 def upload_file():
     try:
         file_path = os.path.join(OUTPUT_FILE_DIR)
-
+        filename_fixer(file_path)
         with open(file_path, 'rb') as f:
             files1 = json.load(f)
             url = 'http://127.0.0.1:8082/'  # need hand with this
