b5b4749ed5cb0734fd924fed678dbcf39c97307b
Jamie Connelly <itsphireyt@gmail.com>
2024-06-27 13:52:46 +0100

    Preparing for final release

--- src/app.py
+++ src/app.py
@@ -1,5 +1,10 @@
-# Basic Flask Server
-# FOR CS356 GROUP PROJECT - GROUP ONE
+# 1KLIK Project - User Interface
+# CS356 Group Project - Group One
+# Abby Boyle, Adam Clacher, Aidan Purdie, James Brown, and Jamie Connelly
+
+# app.py
+# Main Flask server application
+
 import json
 import os
 import random
@@ -8,19 +13,34 @@ import requests
 
 from config_api import ConfigAPI
 from datetime import datetime
-from flask import Flask, render_template, jsonify, request, send_from_directory
+from flask import Flask, render_template, jsonify
 
 # APPLICATION SETTINGS
-SERVER_PORT = 8080
-CONFIG_API_MOCK = True
-CONFIG_API_MOCK_FILE = 'resources/InputConfigJSONTemplate.json'
-FLASK_DEBUG = True
-FLASK_RELOADER = False
-OUTPUT_FILE_DIR = 'resources/output.json'
+SERVER_PORT = 8081                                                     # The port for running the UI SERVER
+FLASK_DEBUG = True                                                     # Use Flask debugger mode
+FLASK_RELOADER = False                                                 # Use Flask reloader
+
+# CONFIG API SETTINGS
+CONFIG_API_URL = '127.0.0.1'                                           # The URL for the CONFIG API SERVER
+CONFIG_API_PORT = '8080'                                               # The port for the CONFIG API SERVER
+CONFIG_API_MOCK = True                                                # Do we want to mock Config API responses?
+CONFIG_API_MOCK_FILE = 'resources/InputConfigJSONTemplate.json'        # If ^ yes, where is the JSON?
+
+# EXPERIMENT API SETTINGS
+EXPERIMENT_API_URL = '127.0.0.1'                                       # The URL for the EXPERIMENT API SERVER
+EXPERIMENT_API_PORT = '8082'                                           # The port for the EXPERIMENT API SERVER
+EXPERIMENT_API_ENDPOINT = '/'                                          # Which endpoint should we hit? (default is root)
+OUTPUT_FILE_DIR = 'resources/output.json'                              # Path of the JSON to send to experiment API
+
 # GLOBAL VARIABLES
 app = Flask(__name__, template_folder='templates/', static_url_path='/static')
+
+# Store Config API instance and cached response
 config_api = 0
 config_api_response = None
+
+
+# ========== FLASK ROUTES ==========
 
 
 @app.route('/')
@@ -78,16 +98,6 @@ def output():
     return render_template('output_page.html', pageName=page_name)
 
 
-@app.route('/sample-conditional')
-def sample_conditional():
-    return render_template('sample_conditional.html', config=config_api_response)
-
-
-@app.route('/api/config', methods=['GET'])
-def get_config():
-    return jsonify(config_api.send_request(None)), 200
-
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
     try:
@@ -96,7 +106,7 @@ def upload_file():
 
         with open(file_path, 'r') as f:
             files1 = json.load(f)
-            url = 'http://127.0.0.1:8082/'  # Test endpoint URL
+            url = f'http://{EXPERIMENT_API_URL}:{EXPERIMENT_API_PORT}{EXPERIMENT_API_ENDPOINT}'
 
             response = requests.post(url, json=files1)
             response.raise_for_status()
@@ -108,11 +118,15 @@ def upload_file():
         return jsonify(success=False, message=f"An error occurred: {str(e)}")
 
 
-def filename_fixer(path):  # doing this as with mock files the experiment team doesn't want duplicates
+# ========== MISCELLANEOUS FUNCTIONS ==========
+
+
+# Doing this as with mock files the experiment team doesn't want duplicates
+def filename_fixer(path):
     with open(path, 'r') as file:
         json_data = json.load(file)
 
-        # Extract the current project name
+    # Extract the current project name
     current_name = json_data["Project"]["name"]
 
     # Generate 2 random characters
@@ -129,31 +143,33 @@ def filename_fixer(path):  # doing this as with mock files the experiment team d
         json.dump(json_data, file, indent=4)
 
 
+# Used to refresh the cached Config API response
 def refresh_config_api_response(endpoint):
+    # We want to modify the global response variable
     global config_api_response
 
     try:
+        # Tell the Config API handler to send a request to the Config API
         config_api_response = config_api.send_request(endpoint, True)
         print(f'[LOG] {datetime.now()} Refreshed Config API Response cache.')
-    except ValueError as e:
-        print(f'[LOG] {datetime.now()} Failed to refresh Config API response cache: {e}')
-    except IOError as e:
-        print(f'[LOG] {datetime.now()} Failed to refresh Config API response cache: {e}')
+    except (ValueError, IOError) as e:
+        print(f'[ERROR] {datetime.now()} Failed to refresh Config API response cache: {e}')
 
 
+# APPLICATION ENTRY POINT
 def main():
     print('[*] 1KLIK PROJECT - USER INTERFACE')
-    print('[*] By Abby, Adam, Aidan, James, and Jamie')
+    print('[*] By Abby, Adam, Aidan, James, and Jamie\n')
     print(f'[LOG] {datetime.now()} Initialising Server...')
     print(f'[LOG] {datetime.now()} CONFIG API SETTINGS: Mock Requests({CONFIG_API_MOCK}), '
           f'Mock File(\'{CONFIG_API_MOCK_FILE}\')')
 
-    # We want to update the global variable
+    # We want to store the Config API instance as a global, so let's update that
     global config_api
 
     try:
         # Set up our ConfigAPI handler
-        config_api = ConfigAPI(CONFIG_API_MOCK, CONFIG_API_MOCK_FILE)
+        config_api = ConfigAPI(CONFIG_API_URL, CONFIG_API_PORT, CONFIG_API_MOCK, CONFIG_API_MOCK_FILE)
     except Exception as e:
         print(f'Failure initialising Config API: {e}')
 
@@ -164,7 +180,7 @@ def main():
 
     # Start the Flask server
     print(f'[LOG] {datetime.now()} FLASK SETTINGS: Debug({FLASK_DEBUG}), Reloader({FLASK_RELOADER})')
-    print(f'[LOG] {datetime.now()} Flask server listening on port {SERVER_PORT}.')
+    print(f'[LOG] {datetime.now()} Flask server listening on port {SERVER_PORT}.\n')
     app.run(debug=FLASK_DEBUG, use_reloader=FLASK_RELOADER, port=SERVER_PORT)
 
 
